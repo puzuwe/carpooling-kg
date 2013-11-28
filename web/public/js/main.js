7ad1e243bd68c0bb66a0c4cf4d7af413b19642d8
Nursultan <nursultan2010@gmail.com>
2013-11-28 23:18:55 +0200

    Bug Fix

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -4,7 +4,7 @@
 $('.form_datetime').datetimepicker({
     //language:  'fr',
     weekStart: 1,
-    todayBtn:  1,
+    todayBtn: 1,
     todayHighlight: 1,
     autoclose: 1,
     todayHighlight: 1,
@@ -13,3 +13,25 @@ $('.form_datetime').datetimepicker({
     showMeridian: 0,
     pickerPosition: "bottom-left"
 });
+
+// Google Maps at home page.
+var mapMain;
+var counterMain=0;
+var beginMain,endMain;
+$(window).load(function () {
+    mapMain = new google.maps.Map(document.getElementById('map-main'),
+        {
+            center: new google.maps.LatLng(42.8747, 74.6122),
+            zoom: 7,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+
+        });
+
+});
+
+//google.maps.event.addDomListener(mapMain,'click',function(){
+//
+//    if(counterMain==0){
+//        beginMain =
+//    }
+//});
