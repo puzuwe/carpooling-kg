d30462d503237b6ced100a306d56b0a55cd14797
Nursultan <nursultan2010@gmail.com>
2014-01-13 03:40:19 +0200

    Language switcher works. Main Content somehow works

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -15,62 +15,72 @@ $('.form_datetime').datetimepicker({
 });
 
 // Google Maps at home page.
-var mapMain;
-var counterMain=0;
-var beginMain,endMain, mainDepartureFilled,mainDestinationFilled;
-var mainMapOptions = {center: new google.maps.LatLng(42.8747, 74.6122), zoom: 7, mapTypeId: google.maps.MapTypeId.ROADMAP};
-var mainGeocoder = new google.maps.Geocoder();
-var mainDirectionsService = new google.maps.DirectionsService();
-var mainMap = new google.maps.Map(document.getElementById('map-main'), mapOptions);
-var directionsDisplay =  new google.maps.DirectionsRenderer();
-directionsDisplay.setMap(mainMap);
-directionsDisplay.setOptions({suppressMarkers: true});
+//var mapMain;
+//var counterMain=0;
+//var beginMain,endMain, mainDepartureFilled,mainDestinationFilled;
+//var mainMapOptions = {center: new google.maps.LatLng(42.8747, 74.6122), zoom: 7, mapTypeId: google.maps.MapTypeId.ROADMAP};
+//var mainGeocoder = new google.maps.Geocoder();
+//var mainDirectionsService = new google.maps.DirectionsService();
+//var mainMap = new google.maps.Map(document.getElementById('map-main'), mapOptions);
+//var directionsDisplay =  new google.maps.DirectionsRenderer();
+//directionsDisplay.setMap(mainMap);
+//directionsDisplay.setOptions({suppressMarkers: true});
+//
+//google.maps.event.addDomListener(mainMap,'click',function(event){
+//
+//    mainGeocoder.geocode({'latLng': event.latLng}, function (results, status) {
+//        if (status == google.maps.GeocoderStatus.OK) {
+//
+//            if (results[1]) {
+//                if(!mainDepartureFilled){
+//                    mainDepartureFilled = true;
+//                    beginMain = event.latLng;
+//                    setMarker(mainMap, results[1].formatted_address, event.latLng, 'a');
+//
+//                    //setLatLng('departure', results[1].formatted_address, event.latLng.lat(), event.latLng.lng());
+//                }else if(!mainDestinationFilled){
+//                    mainDestinationFilled = true;
+//                    endMain = event.latLng;
+//                    setMarker(mainMap, results[1].formatted_address, event.latLng, 'b');
+//                    //setLatLng('destination', results[1].formatted_address, event.latLng.lat(), event.latLng.lng());
+//                }
+//                calcMainRoute(beginMain,endMain);
+//            }
+//        }
+//    });
+//});
 
-google.maps.event.addDomListener(mainMap,'click',function(event){
+//function calcMainRoute(departure,destination) {
+//    if (mainDepartureFilled) {
+//        if (mainDestinationFilled) {
+//            var request = { origin: departure, destination: destination, travelMode: google.maps.TravelMode.DRIVING };
+//            directionsService.route(request, function (response, status) {
+//                if (status == google.maps.DirectionsStatus.OK) {
+//                    directionsDisplay.setDirections(response);
+//                    var datas = {};
+//                    datas.start = response.routes[0].legs[0].steps[0].start_point.toString() + response.routes[0].legs[0].steps[0].end_point.toString();
+//                    datas.end = response.routes[0].legs[0].steps[response.routes[0].legs[0].steps.length - 1].start_point.toString() + response.routes[0].legs[0].steps[response.routes[0].legs[0].steps.length - 1].end_point.toString();
+//                    var urls = '/ru/ajax/rides';
+//                    $.ajax({
+//                            url: urls,
+//                            type:'POST',
+//                            data: datas,
+//                            success:function(response){console.log(response)},
+//                            error:function(){alert(1)}
+//                        }
+//                    );
+//                }
+//            });
+//        }
+//    }
+//}
 
-    mainGeocoder.geocode({'latLng': event.latLng}, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-
-            if (results[1]) {
-                if(!mainDepartureFilled){
-                    mainDepartureFilled = true;
-                    beginMain = event.latLng;
-                    setMarker(mainMap, results[1].formatted_address, event.latLng, 'a');
-
-                    //setLatLng('departure', results[1].formatted_address, event.latLng.lat(), event.latLng.lng());
-                }else if(!mainDestinationFilled){
-                    mainDestinationFilled = true;
-                    endMain = event.latLng;
-                    setMarker(mainMap, results[1].formatted_address, event.latLng, 'b');
-                    //setLatLng('destination', results[1].formatted_address, event.latLng.lat(), event.latLng.lng());
-                }
-                calcMainRoute(beginMain,endMain);
-            }
-        }
-    });
-});
-
-function calcMainRoute(departure,destination) {
-    if (mainDepartureFilled) {
-        if (mainDestinationFilled) {
-            var request = { origin: departure, destination: destination, travelMode: google.maps.TravelMode.DRIVING };
-            directionsService.route(request, function (response, status) {
-                if (status == google.maps.DirectionsStatus.OK) {
-                    directionsDisplay.setDirections(response);
-                    var datas = {};
-                    datas.start = response.routes[0].legs[0].steps[0].start_point.toString() + response.routes[0].legs[0].steps[0].end_point.toString();
-                    datas.end = response.routes[0].legs[0].steps[response.routes[0].legs[0].steps.length - 1].start_point.toString() + response.routes[0].legs[0].steps[response.routes[0].legs[0].steps.length - 1].end_point.toString();
-                    var urls = '/ru/ajax/rides';
-                    $.ajax({
-                            url: urls,
-                            type:'POST',
-                            data: datas,
-                            success:function(response){console.log(response)},
-                            error:function(){alert(1)}
-                        }
-                    );
-                }
-            });
-        }
+$('.language').click(function () {
+    //alert(this.getAttribute('data-link'));
+    var lang = 'US';
+    if (this.getAttribute('data-option') == "ru_RU") {
+        lang = "RU";
     }
-}
+    this.parentNode.parentNode.parentNode.firstChild.nextSibling.firstChild.nextSibling.setAttribute('class', 'glyphicon bfh-flag-' + lang);
+    window.location.href = this.getAttribute('data-link');
+});
